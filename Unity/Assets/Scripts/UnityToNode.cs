2196f81261e218a7c22df351897a4ad79acf38ee
solmin020619 <solmin0619@naver.com>
2025-09-17 13:44:21 +0900

    [Feat] GET POST 통신 테스트

--- Unity/Assets/Scripts/UnityToNode.cs
+++ Unity/Assets/Scripts/UnityToNode.cs
@@ -24,8 +24,8 @@ public class UnityToNode : MonoBehaviour
         btnGetExample.onClick.AddListener(() =>
         {
             var url = string.Format("{0}:{1}/{2}", host, port, route);
+            
             Debug.Log(url);
-
             StartCoroutine(GetData(url, (raw) =>
             {
                 var res = JsonConvert.DeserializeObject<Protocols.Packets.common>(raw);
@@ -82,7 +82,7 @@ public class UnityToNode : MonoBehaviour
         yield return webRequest.SendWebRequest();
 
         if (webRequest.result == UnityWebRequest.Result.ConnectionError                           // 결과 값이 접속 오류일떄
-        || webRequest.result == UnityWebRequest.Result.ProtocolError)                             // 프로토콜 오류 일떄
+            || webRequest.result == UnityWebRequest.Result.ProtocolError)                             // 프로토콜 오류 일떄
         {
             Debug.Log("네트워크 환경이 좋지 않아서 통신 불가");                                   // 통신 안됨 예외처리
         }
